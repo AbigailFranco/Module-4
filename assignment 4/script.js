eaad34bcf6c49eceef57de6f005c1418151b7b72
AbigailFranco <127123125+AbigailFranco@users.noreply.github.com>
2023-04-03 21:20:25 -0400

    Update script.js

--- assignment 4/script.js
+++ assignment 4/script.js
@@ -16,14 +16,3 @@ for (var i = 0; i < names.length; i++) {
 	}
 	else{
 		console.log("Hello "+ names[i])
-	}
-}
-(function(window) {
-	var speakWord = "Hello";
-	var helloSpeaker = function (name) {
-		console.log(speakWord + " " + name);
-	}
-
-	window.helloSpeaker = helloSpeaker;
-
-})(window);
