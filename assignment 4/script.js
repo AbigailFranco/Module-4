d931304006f4c8d9f9ace6ba467da24eadb02fda
AbigailFranco <127123125+AbigailFranco@users.noreply.github.com>
2023-04-03 20:33:32 -0400

    Update script.js

--- assignment 4/script.js
+++ assignment 4/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Yaakov";
+names[0]="Abigail";
 names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
+names[2]="Jenny";
+names[3]="jack";
+names[4]="paula";
+names[5]="franky";
 names[6]="larry";
 names[7]="paula";
 names[8]="laura";
-names[9]="jim";
+names[9]="jimbo";
 
 
 for (var i = 0; i < names.length; i++) {
